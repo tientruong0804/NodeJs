3f84fac5d56aac0d780534fa6020beba6a81a947
tientruong0804 <31621536+tientruong0804@users.noreply.github.com>
2018-10-29 09:43:35 +0700

    Update database.js

--- easy-node-authentication-master/config/database.js
+++ easy-node-authentication-master/config/database.js
@@ -1,5 +1,4 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/tracking_mmgame' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-};
+ };
